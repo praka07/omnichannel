98870455d214eeebf3819eba51e4a18713e468b6
Vrindavan, Nidhin (ITCDEC) - KLM <nidhin.vrindavan@klm.com>
2018-04-02 18:33:22 +0530

    updating seat button

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path    = require("path");
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/design.html'));
+  // console.log("directName"+__dirname);
+}).listen(7080,()=>{
+    console.log("server is running with port 7080");
+});
