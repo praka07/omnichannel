ba24741b5397e37b567769fde2d22a2132b17651
praka07 <33973861+praka07@users.noreply.github.com>
2018-04-03 10:39:37 +0200

    write logic to handle post request

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
-var app = express();
-var path    = require("path");
+var myParser = require("body-parser");
 
+var path    = require("path");
+var app = express();
+app.use(myParser.json());
 app.get('/omnichannel/selection', function (req, res) {
     res.sendFile(path.join(__dirname+'/design.html'));
-  // console.log("directName"+__dirname);
-}).listen(process.env.PORT || 3000,()=>{
-    console.log("server is running with port 7080");
+  });
+app.get('/ominichannel/selection/:uriValue',function(req,res){
+
+    console.log(`request URI:::${req.params.uriValue}`);
+  
+
+});
+app.post('/postpayload',function(req,res){
+   
+    console.log(`payload :::${JSON.stringify(req.body)}`);
+});
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("server is running");
 });
