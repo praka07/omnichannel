feb85fce3a86a7e98ea61f80489ba13ac6f22527
praka07 <33973861+praka07@users.noreply.github.com>
2018-04-04 08:22:20 +0200

    update face message

--- index.js
+++ index.js
@@ -1,16 +1,77 @@
 var express = require('express');
 var myParser = require("body-parser");
+var https = require('https');
 
 var path    = require("path");
 var app = express();
 app.use(myParser.json());
+
+var payload={
+    "recipient": {
+      "id": "1801362176551550"
+    },
+    "message": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "hi, Mani",
+          "buttons": [
+            {
+              "type": "web_url",
+              "url": "https://www.messenger.com",
+              "title": "seat selection"
+            }
+          ]
+        }
+      }
+    }
+  };
+
+
+var postheaders = {
+    'Content-Type' : 'application/json',
+    'Content-Length' : Buffer.byteLength(payload, 'utf8')
+};
+var optionspost = {
+    host : 'graph.facebook.com',
+    port : 443,
+    path : 'v2.6/me/messages?access_token=EAAcMjrnEeygBAJzM3livVqEz6qoNDkJFLRULe9T2Ycn5WAfL2XZBOPRzax9LI433Sm0D7ShGAgFZAcyJmx2By6WTwoid48mvycZCbvUQ3jNESD6Kj3v7EwslqVI3ZBpsn6n0RtDIKDCeXZCLg6LSh1ZAyaqZA5MyC1O6wdZCIKlARgZDZD',
+    method : 'POST',
+    headers : postheaders
+};
+
+
+
+
+
+
+
+
+
 app.get('/omnichannel/selection', function (req, res) {
     res.sendFile(path.join(__dirname+'/design.html'));
   });
-app.get('/ominichannel/selection/:uriValue',function(req,res){
+app.get('/ominichannel/selection/:userId',function(req,res){
 
-    console.log(`request URI:::${req.params.uriValue}`);
-  
+    console.log(`request URI:::${req.params.userId}`);
+	
+	
+	 var reqPost = https.request(optionspost, function(res) {
+        console.log("statusCode: ", res.statusCode);
+         
+        res.on('data', function(d) {
+            console.log(`============POST result:============\n`);
+            process.stdout.write(d);
+            console.log(`\n\nPOST completed`);
+        });
+    });
+     
+    // write the json data
+    reqPost.write(JSON.stringify(payload));
+	reqPost.on('error', function(e) {
+        console.error(e);
+    });
 
 });
 app.post('/postpayload',function(req,res){
@@ -47,6 +108,7 @@ app.post('/omnichannel/facebook/webhook', (req, res) => {
     }
   
   });
-app.listen(process.env.PORT || 3000,()=>{
+  
+  app.listen(process.env.PORT || 3000,()=>{
     console.log("server is running");
 });
