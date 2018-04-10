fe856c1c254d10c0ad63950543d68df2a4a734d8
t303710 <nidhin.vrindavan@klm.com>
2018-04-10 23:02:06 +0530

    changing let to var

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var mLab = require('mongolab-data-api')('w_AGSTEBxndTQ_LTeiFWDS4FVpPM1khT');
 
 app.use(myParser.json());
 
-let bot = new Bot({
+var bot = new Bot({
   token: 'EAAcMjrnEeygBAJzM3livVqEz6qoNDkJFLRULe9T2Ycn5WAfL2XZBOPRzax9LI433Sm0D7ShGAgFZAcyJmx2By6WTwoid48mvycZCbvUQ3jNESD6Kj3v7EwslqVI3ZBpsn6n0RtDIKDCeXZCLg6LSh1ZAyaqZA5MyC1O6wdZCIKlARgZDZD',
   // verify: 'VERIFY_TOKEN',
   app_secret: '164e65ba6bc5a042be106026aa55824f'
@@ -34,7 +34,7 @@ app.post('/ominichannel/sendMessenger', function (req, res) {
     console.log(`fbnumber :::${fbNumber} for flyer number ::: ${req.body.airline_FFNumber}`);
   });
 
-  let temp = { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Hi, Buddy Great to see you here... Please proceed seat selection by clicking the folloeing button", "buttons": [{ "type": "web_url", "url": "https://www.messenger.com", "title": "Continue Journey" }] } } }
+  var temp = { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Hi, Buddy Great to see you here... Please proceed seat selection by clicking the folloeing button", "buttons": [{ "type": "web_url", "url": "https://www.messenger.com", "title": "Continue Journey" }] } } }
   bot.sendMessage(fbNumber, temp, function (err, info) {
     console.log(err)
     console.log(info)
@@ -44,7 +44,7 @@ app.post('/ominichannel/sendMessenger', function (req, res) {
 });
 
 app.get('/omnichannel/facebook/webhook', function (req, res) {
-  let challenge = req.query['hub.challenge'];
+  var challenge = req.query['hub.challenge'];
   console.log(`challenge::${challenge}`);
   res.send(challenge);
 });
@@ -59,7 +59,7 @@ app.get('/omnichannel/alexa/check', function (req, res) {
 
 app.post('/omnichannel/facebook/webhook', (req, res) => {
 
-  let body = req.body;
+  var body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
@@ -69,7 +69,7 @@ app.post('/omnichannel/facebook/webhook', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
+      var webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
 
